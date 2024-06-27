c48d710be262cb7ecf013989fb71665923825951
jmarqu27 <jaime.marquez@outlook.com>
2024-06-27 13:40:34 -0400

    Created a function for creating a new directory

--- image_scraper.py
+++ image_scraper.py
@@ -2,6 +2,29 @@ from bs4 import BeautifulSoup
 import requests
 import os
 
+def create_dir(images):
+    """
+    This function gets user input for a new directory for the images
+    to be downloaded to, then calls the download_images function
+    """
+
+    # Create the new directory
+    try:
+        # Get new directory name from user
+        dir_name = input("Enter the NEW directory: ")
+        os.mkdir(dir_name)
+    except:
+        print("Directory already exists")
+        create_dir(images)
+    
+    # Function call to start downloading images
+    download_images(images, dir_name)
+
+
+def download_images(images, dir_name):
+    pass
+
+
 # Get URL from user
 url = input("Enter the URL: ")
 
@@ -17,5 +40,4 @@ images = soup.find_all("img")
 # Print the list of img tags for visual
 #print(images)
 
-
-
+create_dir(images)
